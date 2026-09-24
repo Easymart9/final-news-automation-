9e596dc442518c669f22e95ee65c4341715f7491
ktaha8967 <ktaha8967@gmail.com>
2026-09-24 20:05:25 +0500

    Remove all files to start a new project

--- src/components/ads/index.ts
+++
@@ -1,5 +0,0 @@
-export { AdsterraBanner728x90 } from './AdsterraBanner728x90';
-export { AdsterraBanner320x50 } from './AdsterraBanner320x50';
-export { AdsterraNativeBanner } from './AdsterraNativeBanner';
-export { AdsterraResponsiveBanner } from './AdsterraResponsiveBanner';
-export { AdsterraSidebarAd } from './AdsterraSidebarAd';
